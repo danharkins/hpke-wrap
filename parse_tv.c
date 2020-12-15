d05403298d58a91a04f18e2b5cdaec23d19ea6cc
Dan Harkins <dharkins@lounge.org>
2020-12-15 14:18:22 -0800

    DNH: forgot copyright notice...

--- parse_tv.c
+++ parse_tv.c
@@ -1,3 +1,35 @@
+/*
+ * Copyright (c) Dan Harkins, 2020
+ *
+ *  Copyright holder grants permission for redistribution and use in source 
+ *  and binary forms, with or without modification, provided that the 
+ *  following conditions are met:
+ *     1. Redistribution of source code must retain the above copyright
+ *        notice, this list of conditions, and the following disclaimer
+ *        in all source files.
+ *     2. Redistribution in binary form must retain the above copyright
+ *        notice, this list of conditions, and the following disclaimer
+ *        in the documentation and/or other materials provided with the
+ *        distribution.
+ *
+ *  "DISCLAIMER OF LIABILITY
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY DAN HARKINS ``AS IS''
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+ *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL DAN HARKINS BE LIABLE 
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ *  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE."
+ *
+ * This license and distribution terms cannot be changed. In other words,
+ * this code cannot simply be copied and put under another distribution
+ * license (including the GNU public license).
+ */
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -5,13 +37,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <openssl/ec.h>
-//#include <openssl/evp.h>
-//#include <openssl/hmac.h>
-//#include <openssl/rand.h>
-//#include <openssl/sha.h>
-#include <openssl/bn.h>
-#include <openssl/aes.h>
 #include "jsmn.h"
 #include "hpke_internal.h"      // because we're mucking with an internal datastructure
                                 // otherwise it should just be hpke.h
